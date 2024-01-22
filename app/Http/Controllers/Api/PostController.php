f8e7630a3dd48f1db3d27295ec738470f0a61c35
giuliamari04 <giuliamari04@gmail.com>
2024-01-22 18:11:41 +0100

    milestone 1

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::paginate(3);
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $posts
+            ]
+        );
+    }
+
+    public function show($slug)
+    {
+        $post = Post::where('slug', $slug)->with(['category', 'technologies'])->first();
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $post
+            ]
+        );
+    }
+}
